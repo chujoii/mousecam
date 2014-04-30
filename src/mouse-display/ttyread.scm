8266d9793c87354f6ba8ef3f341f9d8480022956
Roman V. Prikhodchenko <chujoii@gmail.com>
2014-04-30 21:27:14 +0400

    file header, device path and speed get from main program

--- src/mouse-display/ttyread.scm
+++ src/mouse-display/ttyread.scm
@@ -1,16 +1,51 @@
-#!/usr/bin/env racket
 #lang racket
+
+
+
+;;;; ttyread.scm ---  library for read img from microcontroller
+
+
+
+;;; Copyright (C) 2014 Roman V. Prikhodchenko
+
+
+
+;;; Author: Roman V. Prikhodchenko <chujoii@gmail.com>
+
+
+
+;;;    This file is part of mousecam.
+;;;
+;;;    mousecam is free software: you can redistribute it and/or modify
+;;;    it under the terms of the GNU General Public License as published by
+;;;    the Free Software Foundation, either version 3 of the License, or
+;;;    (at your option) any later version.
+;;;
+;;;    mousecam is distributed in the hope that it will be useful,
+;;;    but WITHOUT ANY WARRANTY; without even the implied warranty of
+;;;    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;;;    GNU General Public License for more details.
+;;;
+;;;    You should have received a copy of the GNU General Public License
+;;;    along with mousecam.  If not, see <http://www.gnu.org/licenses/>.
+
+
+
+;;; Keywords: tty usb microcontroller mouse cam image img
+
+
+
+;;; Usage:
+
+;;; History:
+
+;;; Code:
 
 (require readline)
 (require readline/rep-start)
 
 ;; for config tty
 (require racket/system)
-
-(define *device-path* "/dev/ttyUSB0")
-(define *device-speed* "115200")
-
-
 
 (define tty null)
 
@@ -30,21 +65,18 @@
 
 (define (serial-close-connection)
   ;; additional cleanup here!
-  (printf "fixme")
-)
-
-(define (read-loop)
-  ;(display (bytes->string/utf-8 (read-byte tty)))
-  ;;(display (read-char tty))
-  (let ((qq (read-line tty)))
-    (display "len=")(display (string-length qq))(newline)
-    (display qq)(newline))
-  (read-loop))
+  (printf "fixme"))
 
 
 
-(serial-initialize *device-path* *device-speed*)
-(read-loop)
-(serial-close-connection)
+
+;(serial-initialize *device-path* *device-speed*)
+;(read-loop)
+;(serial-close-connection)
+
+
+
+
+
 
 
